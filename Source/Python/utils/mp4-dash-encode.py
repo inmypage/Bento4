bd214f42382a219d4d387250a675eaa2d91d3c1b
Himadri Nath <himadri_00@live.com>
2020-07-27 08:27:15 +0530

    Update mp4-dash-encode.py
    -map_metadata -1

--- Source/Python/utils/mp4-dash-encode.py
+++ Source/Python/utils/mp4-dash-encode.py
@@ -167,7 +167,7 @@ def main():
     for i in range(options.bitrates):
         output_filename = path.join(options.output_dir, 'video_%05d.mp4' % int(bitrates[i]))
         temp_filename = output_filename+'_'
-        base_cmd  = 'ffmpeg -i %s -strict experimental -codec:a %s -ac 2 -ab %dk -preset slow -map_metadata 0 -codec:v %s' % (quote(args[0]), options.audio_codec, options.audio_bitrate, options.video_codec)
+        base_cmd  = 'ffmpeg -i %s -strict experimental -codec:a %s -ac 2 -ab %dk -preset slow -map_metadata -1 -codec:v %s' % (quote(args[0]), options.audio_codec, options.audio_bitrate, options.video_codec)
         if options.video_codec == 'libx264':
             base_cmd += ' -profile:v baseline'
         if options.text_overlay:
